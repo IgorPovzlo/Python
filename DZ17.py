98248c999789eb6b9c4ad80a6fcbc4e7af97bb34
Igor Povzlo <igpovzlo@gmail.com>
2021-07-04 08:33:51 +0300

    (DZ17 add)

---
+++ DZ17.py
@@ -0,0 +1,20 @@
+print("Домашнее задание №16. Ромб 2")
+rows = int(input("Введите высоту фигуры"))
+if rows % 2 == 0:
+    rows += 1
+cols = rows
+
+for i in range(rows):
+    for j in range(cols):
+        if rows/2 >= i :
+            if j == (rows//2-i) or j == (rows//2+i) or i == rows//2 or  j+i >= rows//2 and j-i<= rows//2  :
+                print("+ ", end="")
+            else:
+                print("  ", end="")
+
+        if rows / 2 < i:
+            if j == i-rows//2 or j == (cols -i-1)+cols//2 or j == rows//2:
+                print("+ ", end="")
+            else:
+                print("  ", end="")
+    print()
